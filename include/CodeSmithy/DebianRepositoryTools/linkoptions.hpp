ef3531ad4d28ec6804843a203ddecf2c0f6f470b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-27 21:59:49 +0000

    Add files

---
+++ include/CodeSmithy/DebianRepositoryTools/linkoptions.hpp
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2020-2024 Xavier Leclercq
+// SPDX-License-Identifier: MIT
+
+#ifndef GUARD_CODESMITHYIDE_DEBIANREPOSITORYTOOLS_LINKOPTIONS_HPP
+#define GUARD_CODESMITHYIDE_DEBIANREPOSITORYTOOLS_LINKOPTIONS_HPP
+
+#ifdef _DEBUG
+#pragma comment(lib, "CodeSmithyDebianRepositoryTools-d.lib")
+#else
+#pragma comment(lib, "CodeSmithyDebianRepositoryTools.lib")
+#endif
+
+#endif
