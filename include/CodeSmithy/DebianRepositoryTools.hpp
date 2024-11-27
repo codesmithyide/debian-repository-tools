7532ddfefbd4b13b7d71a268f9ce15aaee61098a
Xavier Leclercq <xavier@leclercq.dev>
2024-11-27 22:09:57 +0000

    Create library (#1)

---
+++ include/CodeSmithy/DebianRepositoryTools.hpp
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2020-2024 Xavier Leclercq
+// SPDX-License-Identifier: MIT
+
+#ifndef GUARD_CODESMITHYIDE_DEBIANREPOSITORYTOOLS_HPP
+#define GUARD_CODESMITHYIDE_DEBIANREPOSITORYTOOLS_HPP
+
+#include "DebianRepositoryTools/DebianRepositoryManager.hpp"
+#include "DebianRepositoryTools/linkoptions.hpp"
+
+#endif
