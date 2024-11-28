b0b2dc5912d28dd9f3a0d8013883b347b4a95805
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-28 18:51:53 +0000

    Updates

--- library/tests/src/DebianRepositoryManagerTests.cpp
+++ library/tests/src/DebianRepositoryManagerTests.cpp
@@ -28,7 +28,8 @@ void DebianRepositoryManagerTests::GeneratePackagesFileTest1(Ishiko::Test& test)
     test.skip();
 #endif
 
-    const boost::filesystem::path binary_packages_tree_path = test.context().getDataDirectory();
+    const boost::filesystem::path binary_packages_tree_path =
+        test.context().getDataPath("DebianRepositoryManagerTests_GeneratePackagesFileTest1");
     const boost::filesystem::path output_path =
         test.context().getOutputPath("DebianRepositoryManagerTests_GeneratePackagesFileTest1");
 
