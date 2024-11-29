7bc7394f225dd1d851a4c797fe331e7a9d902a5a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-29 22:20:25 +0000

    Updates

--- include/CodeSmithy/DebianRepositoryTools/DebianRepositoryReleaseFile.hpp
+++ include/CodeSmithy/DebianRepositoryTools/DebianRepositoryReleaseFile.hpp
@@ -4,14 +4,34 @@
 #ifndef GUARD_CODESMITHYIDE_DEBIANREPOSITORYTOOLS_DEBIANREPOSITORYRELEASEFILE_HPP
 #define GUARD_CODESMITHYIDE_DEBIANREPOSITORYTOOLS_DEBIANREPOSITORYRELEASEFILE_HPP
 
+#include <Ishiko/Crypto.hpp>
 #include <Ishiko/FileSystem.hpp>
+#include <Ishiko/Time.hpp>
+#include <string>
+#include <vector>
 
 namespace CodeSmithy
 {
     class DebianRepositoryReleaseFile
     {
     public:
+        DebianRepositoryReleaseFile(Ishiko::UTCTime& date);
+
+        void setDate(Ishiko::UTCTime& date);
+        void addFile(const std::string& path);
+
         void write(Ishiko::TextFile& file);
+
+    private:
+        struct FileInfo
+        {
+            Ishiko::SHA256Hash hash;
+            size_t file_size;
+            std::string path;
+        };
+
+        Ishiko::UTCTime& m_date;
+        std::vector<FileInfo> m_files;
     };
 }
 
--- library/src/DebianRepositoryReleaseFile.cpp
+++ library/src/DebianRepositoryReleaseFile.cpp
@@ -5,6 +5,34 @@
 
 using namespace CodeSmithy;
 
+DebianRepositoryReleaseFile::DebianRepositoryReleaseFile(Ishiko::UTCTime& date)
+    : m_date(date)
+{
+}
+
+void DebianRepositoryReleaseFile::setDate(Ishiko::UTCTime& date)
+{
+    m_date = date;
+}
+
+void DebianRepositoryReleaseFile::addFile(const std::string& path)
+{
+    size_t file_size = Ishiko::FileSystem::GetFileSize(path.c_str());
+    m_files.emplace_back(FileInfo{Ishiko::SHA256Hash(), file_size, path});
+    m_files.back().hash.updateFromFile(path);
+}
+
 void DebianRepositoryReleaseFile::write(Ishiko::TextFile& file)
 {
+    file.writeLine("Date: " + m_date.toRFC7231String());
+    if (!m_files.empty())
+    {
+        file.writeLine("SHA256:");
+        for (const FileInfo& info : m_files)
+        {
+            // TODO: the path needs to be relative to the release file path
+            file.writeLine(" " + info.hash.value().toHexString() + " " + std::to_string(info.file_size) + " "
+                + info.path);
+        }
+    }
 }
