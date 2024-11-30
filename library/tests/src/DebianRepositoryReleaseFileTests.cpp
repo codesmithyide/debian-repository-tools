34d9144a105f0b86cf0e66a65f1fdf1ceec84bba
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-30 18:07:16 +0000

    Updates

--- library/tests/src/DebianRepositoryReleaseFileTests.cpp
+++ library/tests/src/DebianRepositoryReleaseFileTests.cpp
@@ -11,11 +11,35 @@ DebianRepositoryReleaseFileTests::DebianRepositoryReleaseFileTests(const Ishiko:
     : Ishiko::TestSequence(number, "DebianRepositoryReleaseFile tests", context)
 {
     append<Ishiko::HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
+    append<Ishiko::HeapAllocationErrorsTest>("write test 1", WriteTest1);
 }
 
 void DebianRepositoryReleaseFileTests::ConstructorTest1(Ishiko::Test& test)
 {
-    DebianRepositoryReleaseFile release_file;
+    DebianRepositoryReleaseFile release_file(Ishiko::UTCTime(Ishiko::Date(2024, 11, 29), Ishiko::TimeOfDay(0, 0, 0)));
 
+    ISHIKO_TEST_PASS();
+}
+
+void DebianRepositoryReleaseFileTests::WriteTest1(Ishiko::Test& test)
+{
+    const char* output_name = "DebianRepositoryReleaseFileTests_WriteTest1";
+    const boost::filesystem::path output_path = test.context().getOutputPath(output_name);
+
+    const boost::filesystem::path packages_file_path = test.context().getDataPath("Packages");
+
+    Ishiko::Error error;
+    Ishiko::TextFile output_file;
+    // TODO: create and use throwing variant of TextFile::Create
+    output_file.create(output_path, error);
+
+    DebianRepositoryReleaseFile release_file(Ishiko::UTCTime(Ishiko::Date(2024, 11, 29), Ishiko::TimeOfDay(0, 0, 0)));
+    release_file.addFile(packages_file_path.string());
+
+    release_file.write(output_file);
+
+    output_file.close();
+
+    ISHIKO_TEST_FAIL_IF_OUTPUT_AND_REFERENCE_FILES_NEQ(output_name);
     ISHIKO_TEST_PASS();
 }
